f499a919e79e21374b44d9a9225f4a00e2ca7666
Shivansh Tiwari <shivanshtiwari6849@gmail.com>
2025-06-05 10:06:46 +0530

    Add examples for tuple creation, manipulation, and iteration in Python

---
+++ day4/converter.py
@@ -0,0 +1,36 @@
+# # converting list, string into tiuples
+
+# l1 =["hi, python Coder"]
+# t1 =tupel(l1)
+# print(t1)
+# peint(type(t1))
+
+# # Stringf into tuples
+
+# s1 = ("EK01", "Eforums")
+# t2 = tuple(l1)
+# print(t1)
+# print(type(t1))
+
+# tupple method
+
+# indeas"
+t1 = ("mumbai","pune","BAnglore","delhi")
+print(t1.count("pune"))
+print(t1.count("Rajkot"))
+
+# enumerate
+
+t1 = ("apple","icecream","elephant", "oop", "umbrella")
+
+for item in enumerate(t1):
+    print(item)
+    
+# iterating through loopp
+
+t1 = ("a","e","i","o","u")
+
+for items in t1:
+    print(items)
+    
+    
---
+++ day4/tuples.py
@@ -0,0 +1,48 @@
+# # Tuple representation
+# t1  =  ()
+# print(t1)
+
+# t3 = "Sid", "Mandlik"
+# print(t1)
+# print(type(t1))
+
+# t2 = ("Sid", "Mandlik", 5, True) 
+# print(t2)
+# print(type(t2))
+
+# # t2[0] = "Mansi"
+# print(t2)
+
+# # concatenation of tuples
+
+# t1 = ("Siddhant", "mandilk")
+# t2 = t1+(1,2,3,4,5)
+
+# print(t2)
+
+# # nesting of tuples
+
+# t3 = (t1, t2)
+# print(t3)
+
+
+# repeatation of tuples
+
+t4 = ("Logic ")*3
+print(t4)
+
+# slicing
+t1 = ("Keyword", "Mouse", "monitor", "printer")
+print(t1[:2]) 
+print(t1[1:]) 
+print(t1[ 0:-3]) 
+print(t1[:]) 
+
+
+# deleting  a tuple
+t1 = ("Keyword", "Mouse", "monitor", "printer")
+# del t1
+
+print(t1)
+
+    
