ec30c47f978eab9aac94bf44c9bc3db94e8fd29a
Shivansh Tiwari <shivanshtiwari6849@gmail.com>
2025-06-06 08:25:37 +0530

    Add timeit examples for tuple and list creation; implement tuple methods for language analysis

---
+++ day4/timeitfun.py
@@ -0,0 +1,3 @@
+import timeit
+print(timeit.timeit("x = (1,2,3,4)", number = 1000000))
+print(timeit.timeit("x=[1,2,3,4,5]",number = 1000000))
---
+++ day4/tuple_method.py
@@ -0,0 +1,9 @@
+language=('c','java','c++','vb','phython','php','25')
+print(language)
+print (max(language))#highest ascii value
+print(min(language))#lowest ascii value
+var=sorted(language)#sorts according to ascii
+print(var)
+val=(1,2,3,4,5,6,False)
+print(sum(val))
+print(any((7,)))#returns true if any element exists
