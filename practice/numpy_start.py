73b1ac1eebad46f614a00217e35b9fd0578c073a
Shivansh Tiwari <shivanshtiwari6849@gmail.com>
2025-06-09 23:31:50 +0530

    Refactor numpy array examples and add basic statistics calculations

--- practice/numpy_start.py
+++ practice/numpy_start.py
@@ -1,3 +1,12 @@
-import numpy
-arr = numpy.array([1,2,3,4,5])
+import numpy as np
+arr = np.array([1,2,3,4,5])
+arr2 = np.array([[1,2,3,4,5],[3,5,6,7,8]])
+
 print(arr)
+
+print(np.__version__)
+
+print(type(arr))
+print(arr.ndim)
+print(arr2.ndim)
+
